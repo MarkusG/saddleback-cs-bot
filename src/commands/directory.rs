6f3e950bc5b88d7123b1757ddef306dd9da01e8c
Mark Gross <mark.gross2001@gmail.com>
2020-08-23 13:59:41 -0700

    Implement name command
    Allow users to set their first and last name

--- src/commands/directory.rs
+++ src/commands/directory.rs
@@ -9,13 +9,8 @@ use saddleback_cs_bot::DbConnection;
 
 #[command]
 pub async fn name(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let response;
     let first = args.single::<String>()?;
-    if let Ok(s) = args.single::<String>() {
-        response = format!("Hello, {} {}!", first, s);
-    } else {
-        response = format!("Hello, {}!", first);
-    }
+    let last = args.single::<String>()?;
 
     // get the db connection from context
     let db_client_lock = ctx.data.read().await.get::<DbConnection>().cloned().unwrap();
@@ -23,14 +18,19 @@ pub async fn name(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     let db_client = db_client_lock.lock().await;
     // ensure our connection is present and run our query
     if let Some(c) = &*db_client {
+        let query = r#"INSERT INTO member (id, first_name, last_name)
+            VALUES ($1, initcap($2), initcap($3))
+            ON CONFLICT (id) DO UPDATE
+            SET first_name = excluded.first_name,
+                last_name = excluded.last_name"#;
         let rows = c
-            .query("SELECT * from member", &[])
+            .query(query, &[&(*msg.author.id.as_u64() as i64), &first, &last])
             .await;
         match rows {
             Ok(r) => {
                 for row in r {
-                    let first_name: &str = row.get(2);
-                    let last_name: &str = row.get(3);
+                    let first_name: &str = row.get(0);
+                    let last_name: &str = row.get(1);
                     println!("{} {}", first_name, last_name);
                 }
             },
@@ -41,7 +41,7 @@ pub async fn name(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
 
     // worry about casing in postgres
 
-    if let Err(e) = msg.channel_id.say(&ctx.http, response).await {
+    if let Err(e) = msg.channel_id.say(&ctx.http, "OK").await {
         println!("Error in command add: {:?}", e);
     }
 
--- tables.sql
+++ tables.sql
@@ -1,8 +1,13 @@
+CREATE USER saddlebot;
+GRANT SELECT ON member TO saddlebot;
+GRANT INSERT ON member TO saddlebot;
+GRANT UPDATE ON member TO saddlebot;
+
 CREATE TYPE course AS ENUM ('1A', '1B', '1C', '1D', '3A', '3B', '4A', 'CIMS140', 'CIMS150');
 
 CREATE TABLE member (
 	id           BIGINT PRIMARY KEY,
-	is_professor BOOLEAN NOT NULL,
+	is_professor BOOLEAN NOT NULL DEFAULT false,
 	first_name   TEXT,
 	last_name    TEXT,
 	courses      course[]
